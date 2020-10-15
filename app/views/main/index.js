06c0cd9473971740f2305e9b93cf9a2dfc929e34
Ainars Ciesa <ainars.c@gmail.com>
2020-10-15 11:02:24 -0600

    Add some code

---
+++ app/views/main/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, ScrollView, Text, StatusBar} from 'react-native';
+
+const index = () => {
+  return (
+    <>
+      <StatusBar backgroundColor="#111e6c" />
+      <ScrollView>
+        <View>
+          <Text> FOO Main FOO</Text>
+          <Text> FOO Main FOO</Text>
+          <Text> FOO Main FOO</Text>
+          <Text> FOO Main FOO</Text>
+          <Text> FOO Main FOO</Text>
+          <Text> FOO Main FOO</Text>
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+export default index;
