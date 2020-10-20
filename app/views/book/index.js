fbe202400f22fa1e86dc689d6f42ab6eccc85cea
Ainars Ciesa <ainars.c@gmail.com>
2020-10-19 18:04:31 -0600

    Make the streamer work, still buggy

--- app/views/book/index.js
+++ app/views/book/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View} from 'react-native';
 import BookStyle from './styles';
-import {Epub} from '@ottofeller/epubjs-rn';
+import {Epub, Streamer} from '@ottofeller/epubjs-rn';
 import themes from './reader.json';
 
 const Book = () => {
+  const [book, setBook] = useState('');
+  const FLOW = 'paginated';
+  const BOOK_URL = 'https://s3.amazonaws.com/epubjs/books/moby-dick.epub';
+  let streamer = new Streamer({port: '8899', root: 'assets'});
+  useEffect(() => {
+    streamer
+      .start(8899) // Runs the server
+      .then(() => streamer.get(BOOK_URL))
+      .then((modifiedPath) => setBook(modifiedPath));
+    return () => streamer.kill();
+  }, [BOOK_URL, book, streamer]);
+
   return (
     <View style={BookStyle.container}>
-      <Epub
-        src={'https://s3.amazonaws.com/epubjs/books/moby-dick/OPS/package.opf'}
-        flow={'paginated'}
-        themes={themes}
-        theme={'dark'}
-      />
+      <Epub src={book} flow={FLOW} themes={themes} theme={'dark'} />
     </View>
   );
 };
